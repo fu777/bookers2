057473c0775933609d97000aa6c367a1ea43240b
EC2 Default User <ec2-user@ip-172-31-20-48.ec2.internal>
2022-03-23 12:22:45 +0000

    [update] 4-25-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,42 @@
 class BooksController < ApplicationController
   
-  def new
-    @book = Book.new
-  end
-  
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    @user = current_user
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to books_path
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def show
+    @book = Book.new
+    @book = Book.find(params[:id])
+    @user = current_user
+  end
+  
+  def edit
     @book = Book.find(params[:id])
   end
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(book.id)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
